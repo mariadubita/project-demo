b072cf3bd34900cdd7c26bcb41cb2fb314fb4d2b
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-12 16:47:11 -0500

    vnet added

---
+++ vnet.tf
@@ -0,0 +1,8 @@
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "wordpress" {
+ name               = "wordpress-vnet"
+ address_space      = ["10.0.0.0/16"]
+ location           = var.location
+ resource_group_name = azurerm_resource_group.wordpress.name
+ tags               = var.tags
+}
