557720af04f6315e220e0cb9e79bdd580a9b7cf2
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-12 16:22:36 -0500

    random string

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "wordpress" {
  location = var.location
  tags   = var.tags
 }
+
+# Generates a random permutation of alphanumeric characters
+resource "random_string" "fqdn" {
+  length  = 6
+  special = false
+  upper   = false
+  numeric  = false
+}
