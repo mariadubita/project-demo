d6f90964e8c71bebe5d4be5d7cfc05cad89528d6
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-12 16:53:26 -0500

    edited

--- vnet.tf
+++ vnet.tf
@@ -9,21 +9,21 @@ resource "azurerm_virtual_network" "wordpress" {
 
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet2"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_subnet" "subnet3" {
   name                 = "subnet3"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.3.0/24"]
 }
