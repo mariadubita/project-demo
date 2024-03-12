cdc3fe66e33e8a5b4e2d9de02b854380d274ab14
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-12 17:51:21 -0500

    .

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,3 +1,16 @@
+# Define Azure resource group
+resource "azurerm_resource_group" "wordpress" {
+  name     = "wordpress-resources"
+  location = var.location
+}
+# Declare the public IP address resource
+resource "azurerm_public_ip" "wordpress" {
+  name                = "wordpress-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+  allocation_method   = "Dynamic"
+}
+# Define Azure Load Balancer
 resource "azurerm_lb" "wordpress" {
   name                = "wordpress-lb"
   location            = var.location
@@ -8,18 +21,19 @@ resource "azurerm_lb" "wordpress" {
   }
   tags = var.tags
 }
+# Define backend address pool for the Load Balancer
 resource "azurerm_lb_backend_address_pool" "bpepool" {
   loadbalancer_id = azurerm_lb.wordpress.id
   name            = "BackEndAddressPool"
 }
+# Define probe for the Load Balancer
 resource "azurerm_lb_probe" "wordpress" {
-  resource_group_name = azurerm_resource_group.wordpress.name
-  loadbalancer_id     = azurerm_lb.wordpress.id
-  name                = "ssh-running-probe"
-  port                = var.application_port
+  loadbalancer_id = azurerm_lb.wordpress.id
+  name            = "ssh-running-probe"
+  port            = var.application_port
 }
+# Define rule for the Load Balancer
 resource "azurerm_lb_rule" "lbnatrule" {
-  resource_group_name            = azurerm_resource_group.wordpress.name
   loadbalancer_id                = azurerm_lb.wordpress.id
   name                           = "http"
   protocol                       = "Tcp"
@@ -28,4 +42,5 @@ resource "azurerm_lb_rule" "lbnatrule" {
   backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = azurerm_lb_probe.wordpress.id
+  resource_group_name = azurerm_resource_group.wordpress.name
 }
