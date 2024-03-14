ef7474e578261591a58b63f58bae432b80afecf5
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-14 16:27:51 -0500

    rg id

--- output.tf
+++ output.tf
@@ -6,9 +6,6 @@ output "application_public_address" {
   value = aws_route53_record.wordpressdb.name
 }
 
-output rg_id {
-    value = azurerm_resource_group.example.id
-}
 output sql_id {
     value = azurerm_mysql_server.example.id
 }
