6909f6e8cbd7c9131065bf7d8e57b1fb0e595e36
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-14 16:29:57 -0500

    d

--- output.tf
+++ output.tf
@@ -5,10 +5,3 @@ output vnet_id {
 output "application_public_address" {
   value = aws_route53_record.wordpressdb.name
 }
-
-output sql_id {
-    value = azurerm_mysql_server.example.id
-}
-output ss_id {
-    value = azurerm_linux_virtual_machine_scale_set.example.id
-}
