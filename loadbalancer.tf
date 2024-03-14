af6b5251936ed2ec8af84ee5a5d9f6fa5e305513
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-14 15:03:44 -0500

    t

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -28,13 +28,12 @@ resource "azurerm_lb_probe" "wordpress" {
 
 # Define the Load Balancer Rule
 resource "azurerm_lb_rule" "lbnatrule" {
-  resource_group_name            = azurerm_resource_group.wordpress.name
   loadbalancer_id                = azurerm_lb.wordpress.id
   name                           = "http"
   protocol                       = "Tcp"
   frontend_port                  = var.application_port
   backend_port                   = var.application_port
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  backend_address_pool_id        = [azurerm_lb_backend_address_pool.bpepool.id]
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = azurerm_lb_probe.wordpress.id
 }
