003682dda03b2ad94054db68f185b135aaeb91bc
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-14 15:23:23 -0500

    variable

--- variable.tf
+++ variable.tf
@@ -16,3 +16,14 @@ variable "application_port" {
   description = "The port that you want to expose to the external load balancer"
   default     = 80
 }
+
+variable "admin_username" {
+  description = "User name to use as the admin account on the VMs that will be part of the VM Scale Set"
+  default     = "wordpress"
+}
+
+
+variable "admin_password" {
+  description = "Default password for admin account"
+  default     = "26F4QXHVYbBjC$WH2HAc"
+}
