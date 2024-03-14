d167ed5a76afe31ac5a582fd6f63c09b4a803772
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-14 19:48:30 +0000

    lb

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,3 +1,13 @@
+# Define the Azure Public IP address for WordPress
+resource "azurerm_public_ip" "wordpress" {
+  name                = "wordpress-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+  allocation_method   = "Dynamic"
+}
+
+
+# Define the Azure Load Balancer for WordPress
 resource "azurerm_lb" "wordpress" {
   name                = "wordpress-lb"
   location            = var.location
@@ -8,16 +18,25 @@ resource "azurerm_lb" "wordpress" {
   }
   tags = var.tags
 }
+
+
+# Define the Backend Address Pool for the Load Balancer
 resource "azurerm_lb_backend_address_pool" "bpepool" {
   loadbalancer_id = azurerm_lb.wordpress.id
   name            = "BackEndAddressPool"
 }
+
+
+# Define the Probe for the Load Balancer
 resource "azurerm_lb_probe" "wordpress" {
   resource_group_name = azurerm_resource_group.wordpress.name
   loadbalancer_id     = azurerm_lb.wordpress.id
   name                = "ssh-running-probe"
   port                = var.application_port
 }
+
+
+# Define the Load Balancer Rule
 resource "azurerm_lb_rule" "lbnatrule" {
   resource_group_name            = azurerm_resource_group.wordpress.name
   loadbalancer_id                = azurerm_lb.wordpress.id
